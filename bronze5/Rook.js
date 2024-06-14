50aadb8d460c9f4aae228047358bd3a6eb97d3dc
jaceleedev <jaceleedev@gmail.com>
2024-06-14 16:34:38 +0900

    feat: :technologist: solve bronze5 Rook

---
+++ bronze5/Rook.js
@@ -0,0 +1,14 @@
+// Rook
+console.log('  ___  ___  ___');
+console.log('  | |__| |__| |');
+console.log('  |           |');
+console.log('   \\_________/');
+console.log('    \\_______/');
+console.log('     |     |');
+console.log('     |     |');
+console.log('     |     |');
+console.log('     |     |');
+console.log('     |_____|');
+console.log('  __/       \\__');
+console.log(' /             \\');
+console.log('/_______________\\');
