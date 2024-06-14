e41e1f63c73ec30a1774a7a525effa6d6509f379
jaceleedev <jaceleedev@gmail.com>
2024-06-14 10:55:23 +0900

    feat: :technologist: solve bronze5 A+B

--- 1. 입출력과 사칙연산/2. 1000.js
+++
@@ -1,5 +0,0 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
-
-const [a, b] = input[0].split(' ');
-
-console.log(Number(a) + Number(b));
---
+++ bronze5/A+B.js
@@ -0,0 +1,10 @@
+// A+B
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const [a, b] = input[0].split(' ').map(Number);
+
+console.log(a + b);
