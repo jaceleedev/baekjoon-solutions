736c91c99a4b4374613b2339c56dfc274ed83451
jaceleedev <jaceleedev@gmail.com>
2024-06-14 11:10:54 +0900

    feat: :technologist: solve bronze5 엄청난 부자2

---
+++ bronze5/엄청난 부자2.js
@@ -0,0 +1,11 @@
+// 엄청난 부자2
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const [m, n] = input[0].split(' ').map(BigInt);
+
+console.log((m / n).toString());
+console.log((m % n).toString());
