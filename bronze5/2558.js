2038d846fa72491b9b1f4ca08404893388be0a88
jaceleedev <jaceleedev@gmail.com>
2024-06-17 21:36:57 +0900

    feat: :technologist: solve bronze5 A+B - 2 (2558)

---
+++ bronze5/2558.js
@@ -0,0 +1,11 @@
+// A+B - 2 (2558)
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const a = Number(input[0]);
+const b = Number(input[1]);
+
+console.log(a + b);
