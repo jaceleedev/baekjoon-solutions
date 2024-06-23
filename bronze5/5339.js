f88d87aac53a6ed4a0af78e9e4a8498ac65768f0
jaceleedev <jaceleedev@gmail.com>
2024-06-23 20:30:55 +0900

    feat: :technologist: solve bronze5 콜센터 (5339)

---
+++ bronze5/5339.js
@@ -0,0 +1,11 @@
+// 콜센터 (5339)
+console.log(`     /~\\`);
+console.log(`    ( oo|`);
+console.log(`    _\\=/_`);
+console.log(`   /  _  \\`);
+console.log(`  //|/.\\|\\\\`);
+console.log(` ||  \\ /  ||`);
+console.log(`============`);
+console.log(`|          |`);
+console.log(`|          |`);
+console.log(`|          |`);
