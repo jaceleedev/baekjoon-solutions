e003569854a18d28f5de2d416839ff6def5f820b
jaceleedev <jaceleedev@gmail.com>
2024-06-24 22:25:19 +0900

    feat: :technologist: solve bronze5 문자열 (9086)

--- bronze5/9086.js
+++ bronze5/9086.js
@@ -1,3 +1,4 @@
+// 문자열 (9086)
 const input = require('fs')
   .readFileSync('/dev/stdin')
   .toString()
@@ -5,12 +6,14 @@ const input = require('fs')
   .split('\n');
 
 const t = Number(input[0]);
-
 let answer = '';
+
 for (let i = 1; i <= t; ++i) {
   const word = input[i];
+  const first = word[0];
+  const last = word[word.length - 1];
 
-  answer += `${word.charAt(0)}${word.charAt(word.length - 1)}\n`;
+  answer += `${first}${last}\n`;
 }
 
 console.log(answer);
