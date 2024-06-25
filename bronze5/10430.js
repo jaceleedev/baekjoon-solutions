cdded281a0e7ca579992a22395a9896ef99c0b32
jaceleedev <jaceleedev@gmail.com>
2024-06-25 14:21:27 +0900

    feat: :technologist: solve bronze5 나머지 (10430)

---
+++ bronze5/10430.js
@@ -0,0 +1,13 @@
+// 나머지 (10430)
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const [a, b, c] = input[0].split(' ').map(Number);
+
+console.log((a + b) % c);
+console.log(((a % c) + (b % c)) % c);
+console.log((a * b) % c);
+console.log(((a % c) * (b % c)) % c);
