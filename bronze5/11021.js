c4289c26c66a5a081401320594512f9c1955e43a
jaceleedev <jaceleedev@gmail.com>
2024-07-02 15:18:47 +0900

    feat: :technologist: solve bronze5 A+B - 7 (11021)

---
+++ bronze5/11021.js
@@ -0,0 +1,14 @@
+// A+B - 7 (11021)
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const t = Number(input[0]);
+
+for (let i = 1; i <= t; ++i) {
+  const [a, b] = input[i].split(' ').map(Number);
+
+  console.log(`Case #${i}: ${a + b}`);
+}
