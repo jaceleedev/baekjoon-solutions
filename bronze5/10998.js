c514e8f4c358f6d5794ba778458ea2814a165630
jaceleedev <jaceleedev@gmail.com>
2024-07-02 15:16:34 +0900

    feat: :technologist: solve bronze5 A×B (10998)

---
+++ bronze5/10998.js
@@ -0,0 +1,10 @@
+// A×B (10998)
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const [a, b] = input[0].split(' ').map(Number);
+
+console.log(a * b);
