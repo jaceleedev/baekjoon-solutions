5db62f75b581b38ceaeea658b4f02c161d30925e
jaceleedev <jaceleedev@gmail.com>
2024-07-04 10:00:21 +0900

    feat: :technologist: solve bronze5 A+B - 9 (15740)

---
+++ bronze5/15740.js
@@ -0,0 +1,10 @@
+// A+B - 9 (15740)
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const [a, b] = input[0].split(' ').map(BigInt);
+
+console.log((a + b).toString());
