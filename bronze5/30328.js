d311541054a93a7b7d5c379c5ac4e68742762568
jaceleedev <jaceleedev@gmail.com>
2024-07-14 01:32:33 +0900

    feat: :technologist: solve bronze5 Java Warriors (30328)

---
+++ bronze5/30328.js
@@ -0,0 +1,10 @@
+// Java Warriors (30328)
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const n = Number(input[0]);
+
+console.log(n * 4000);
