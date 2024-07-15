443276cc8b520c2b39bbceba74b7f54a050daa11
jaceleedev <jaceleedev@gmail.com>
2024-07-15 14:55:58 +0900

    feat: :technologist: solve bronze4 별 찍기 - 3 (2440)

---
+++ bronze4/2440.js
@@ -0,0 +1,14 @@
+// 별 찍기 - 3 (2440)
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const n = Number(input[0]);
+
+for (let i = n; i >= 1; --i) {
+  const stars = '*'.repeat(i);
+
+  console.log(stars);
+}
