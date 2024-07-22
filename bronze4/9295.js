7503477ad4e89766953109cb0609d4296fdb4fa0
jaceleedev <jaceleedev@gmail.com>
2024-07-22 20:36:06 +0900

    feat: :technologist: solve bronze4 주사위 (9295)

---
+++ bronze4/9295.js
@@ -0,0 +1,14 @@
+// 주사위 (9295)
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const t = Number(input[0]);
+
+for (let i = 1; i <= t; ++i) {
+  const [a, b] = input[i].split(' ').map(Number);
+
+  console.log(`Case ${i}: ${a + b}`);
+}
