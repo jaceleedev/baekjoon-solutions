747b67015b6e8c757948ba9f0e321fb4cd01f296
jaceleedev <jaceleedev@gmail.com>
2024-07-24 14:29:21 +0900

    feat: :technologist: solve bronze4 Oddities (10480)

---
+++ bronze4/10480.js
@@ -0,0 +1,18 @@
+// Oddities (10480)
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const n = Number(input[0]);
+
+for (let i = 1; i <= n; ++i) {
+  const number = Number(input[i]);
+
+  if (number % 2 === 0) {
+    console.log(`${number} is even`);
+  } else {
+    console.log(`${number} is odd`);
+  }
+}
