427a4018a001221637c1a3ddee71e279686ae5ad
jaceleedev <jaceleedev@gmail.com>
2024-08-18 21:27:10 +0900

    feat: :technologist: solve bronze4 SMS from MCHS (21638)

---
+++ bronze4/21638.js
@@ -0,0 +1,21 @@
+// SMS from MCHS (21638)
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const [t1, v1] = input[0].split(' ').map(Number);
+const [t2, v2] = input[1].split(' ').map(Number);
+
+if (t2 < 0 && v2 >= 10) {
+  console.log(
+    `A storm warning for tomorrow! Be careful and stay home if possible!`
+  );
+} else if (t1 > t2) {
+  console.log(`MCHS warns! Low temperature is expected tomorrow.`);
+} else if (v1 < v2) {
+  console.log(`MCHS warns! Strong wind is expected tomorrow.`);
+} else {
+  console.log(`No message`);
+}
