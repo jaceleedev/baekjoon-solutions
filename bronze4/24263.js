ee01ed70c6d5a8bda4f420faf1429a93f26972d5
jaceleedev <jaceleedev@gmail.com>
2024-08-25 14:36:28 +0900

    feat: :technologist: solve bronze4 알고리즘 수업 - 알고리즘의 수행 시간 2 (24263)

---
+++ bronze4/24263.js
@@ -0,0 +1,11 @@
+// 알고리즘 수업 - 알고리즘의 수행 시간 2 (24263)
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const n = Number(input[0]);
+
+console.log(n);
+console.log(1);
